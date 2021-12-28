9fa9f10e201fb998794b2c1540e67223f3c6ed87
Kost36 <60645451+Kost36@users.noreply.github.com>
2021-12-28 15:45:38 +0300

    fix product

--- Game/Components/Products/Products.cs
+++ Game/Components/Products/Products.cs
@@ -2,79 +2,114 @@
 
 namespace Game.Components.Products
 {
-    ///// <summary>
-    ///// Продукт
-    ///// </summary>
-    //public abstract class Product : Component
-    //{
-    //    /// <summary>
-    //    /// Количество товара
-    //    /// </summary>
-    //    public int Count;
-    //    /// <summary>
-    //    /// Вес (кг.)
-    //    /// </summary>
-    //    public float Weight;
-    //    /// <summary>
-    //    /// Объем (м.куб.)
-    //    /// </summary>
-    //    public float Volume;
-    //}
+    /// <summary>
+    /// Продукт
+    /// </summary>
+    public abstract class Product : Component
+    {
+        /// <summary>
+        /// Количество товара
+        /// </summary>
+        public int Count;
+        /// <summary>
+        /// Цена покупки товара
+        /// </summary>
+        public float PriceBuy;
+        /// <summary>
+        /// Цена продажи товара
+        /// </summary>
+        public float PriceSell;
 
-    ///// <summary>
-    ///// Аккумулятор / батарея с энергией
-    ///// </summary>
-    //public class Battery : Product { }
 
-    ////Сырьё 0 уровень (Добываемое)
-    ///// <summary>
-    ///// Руда
-    ///// </summary>
-    //public class Ore : Product { }
-    ///// <summary>
-    ///// Кремний
-    ///// </summary>
-    //public class Silicon : Product { }
-    ///// <summary>
-    ///// Метан
-    ///// </summary>
-    //public class Methane : Product { }
-    ///// <summary>
-    ///// Водород
-    ///// </summary>
-    //public class Hydrogen : Product { }
-    ///// <summary>
-    ///// Гелий
-    ///// </summary>
-    //public class Helium : Product { }
+        public abstract float Weight { get; }
+        public abstract float Volume { get; }
+    }
 
-    ////Продукты 1 уровня (Производимое)
-    ///// <summary>
-    ///// Железо
-    ///// </summary>
-    //public class Iron : Product { }
-    ///// <summary>
-    ///// Пластмасса
-    ///// </summary>
-    //public class Polymer : Product { }
+    /// <summary>
+    /// Аккумулятор / батарея с энергией
+    /// </summary>
+    public class Battery : Product
+    {
+        private static readonly float _weight = 0;
+        private static readonly float _volume = 0;
 
-    ////Продукты 2 уровня (Производимое)
-    ///// <summary>
-    ///// Пластик
-    ///// </summary>
-    //public class Plastic : Product { }
-    ///// <summary>
-    ///// Резина
-    ///// </summary>
-    //public class Rubber : Product { }
+        /// <summary>
+        /// Вес одной еденицы товара.
+        /// Кг.
+        /// </summary>
+        public override float Weight  => _weight;
+        /// <summary>
+        /// Объем одной еденицы товара.
+        /// М.Куб.
+        /// </summary>
+        public override float Volume => _volume;
+    }
 
-    //Продукты 3 уровня (Производимые)
+    //Сырьё 0 уровень (Добываемое)
+    /// <summary>
+    /// Руда
+    /// </summary>
+    public class Ore : Product
+    {
+        private static readonly float _weight = 0;
+        private static readonly float _volume = 0;
 
-    //Продукты 4 уровня (Производимые) => Высокотехнологичные
+        /// <summary>
+        /// Вес одной еденицы товара.
+        /// Кг.
+        /// </summary>
+        public override float Weight => _weight;
 
-    //Продукты 5 уровня (Производимые) => Оружие, дроны, турели, модули
+        /// <summary>
+        /// Вес одной еденицы товара.
+        /// Кг.
+        /// </summary>
+        public override float Volume => _volume;
+    }
+    /// <summary>
+    /// Кремний
+    /// </summary>
+    public class Silicon : Product { }
+    /// <summary>
+    /// Метан
+    /// </summary>
+    public class Methane : Product { }
+    /// <summary>
+    /// Водород
+    /// </summary>
+    public class Hydrogen : Product { }
+    /// <summary>
+    /// Гелий
+    /// </summary>
+    public class Helium : Product { }
 
-    //Продукты 6 уровня (Производимые) => Станции, корабли
+    //Продукты 1 уровня (Производимое)
+    /// <summary>
+    /// Железо
+    /// </summary>
+    public class Iron : Product { }
+    /// <summary>
+    /// Пластмасса
+    /// </summary>
+    public class Polymer : Product { }
+
+    //Продукты 2 уровня (Производимое)
+    /// <summary>
+    /// Пластик
+    /// </summary>
+    public class Plastic : Product { }
+    /// <summary>
+    /// Резина
+    /// </summary>
+    public class Rubber : Product { }
+
+    Продукты 3 уровня(Производимые)
+
+    Продукты 4 уровня(Производимые) => Высокотехнологичные
+
+    Продукты 5 уровня(Производимые) => Оружие, дроны, турели, модули
+
+   Продукты 6 уровня(Производимые) => Станции, корабли
 }
 
 /* //Код для копирования, при создании продукта

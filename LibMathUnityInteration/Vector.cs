6649f97993d859cba1eace8908b2d1b7e401367c
Kost36 <60645451+Kost36@users.noreply.github.com>
2022-04-17 12:10:27 +0300

    Clear

--- LibMathUnityInteration/Vector.cs
+++ LibMathUnityInteration/Vector.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace LibMath
+namespace GameLib.LibHelp
 {
     /// <summary>
     /// Вектор
@@ -8,12 +12,27 @@ namespace LibMath
     public class Vector
     {
         #region Конструкторы
+        /// <summary>
+        /// Инициализация вектора
+        /// </summary>
+        /// <param name="dX"> Длинна по оси X </param>
+        /// <param name="dY"> Длинна по оси Y </param>
+        /// <param name="dZ"> Длинна по оси Z </param>
         public Vector(float dX, float dY, float dZ) { X = dX; Y = dY; Z = dZ; }
         #endregion
 
         #region Свойства
+        /// <summary>
+        /// Длинна по оси X
+        /// </summary>
         public float X { get; set; }
+        /// <summary>
+        /// Длинна по оси Y
+        /// </summary>
         public float Y { get; set; }
+        /// <summary>
+        /// Длинна по оси Z
+        /// </summary>
         public float Z { get; set; }
         #endregion
 
@@ -21,9 +40,9 @@ namespace LibMath
         /// <summary>
         /// Длинна вектора.
         /// </summary>
-        /// <param name="dX"> Дельта по оси X </param>
-        /// <param name="dY"> Дельта по оси Y </param>
-        /// <param name="dZ"> Дельта по оси Z </param>
+        /// <param name="dX"> Длинна по оси X </param>
+        /// <param name="dY"> Длинна по оси Y </param>
+        /// <param name="dZ"> Длинна по оси Z </param>
         /// <returns> длинна  </returns>
         public static float Len(float dX, float dY, float dZ)
         {
@@ -32,19 +51,27 @@ namespace LibMath
         /// <summary>
         /// Длинна вектора.
         /// </summary>
-        /// <param name="vector"> Вектор направления </param>
-        /// <returns> длинна </returns>
+        /// <param name="vector"> Вектор </param>
+        /// <returns> Длинна </returns>
         public static float Len(Vector vector)
         {
             return (float)Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2) + Math.Pow(vector.Z, 2));
+        }
+        /// <summary>
+        /// Длинна вектора.
+        /// </summary>
+        /// <returns> Длинна </returns>
+        public float Len()
+        {
+            return (float)Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
         }
 
         /// <summary>
         /// Нормализация вектора.
         /// </summary>
-        /// <param name="dX"> Дельта по оси X </param>
-        /// <param name="dY"> Дельта по оси Y </param>
-        /// <param name="dZ"> Дельта по оси Z </param>
+        /// <param name="dX"> Длинна по оси X </param>
+        /// <param name="dY"> Длинна по оси Y </param>
+        /// <param name="dZ"> Длинна по оси Z </param>
         /// <returns> Нормализованный вектор </returns>
         public static Vector Norm(float dX, float dY, float dZ)
         {
@@ -54,14 +81,23 @@ namespace LibMath
         /// <summary>
         /// Нормализация вектора.
         /// </summary>
-        /// <param name="dX"> Дельта по оси X </param>
-        /// <param name="dY"> Дельта по оси Y </param>
-        /// <param name="dZ"> Дельта по оси Z </param>
+        /// <param name="dX"> Длинна по оси X </param>
+        /// <param name="dY"> Длинна по оси Y </param>
+        /// <param name="dZ"> Длинна по оси Z </param>
         /// <param name="len"> Длинна вектора </param>
         /// <returns> Нормализованный вектор </returns>
         public static Vector Norm(float dX, float dY, float dZ, float len)
         {
             return new Vector(dX / len, dY / len, dZ / len);
+        }
+        /// <summary>
+        /// Нормализация вектора.
+        /// </summary>
+        /// <returns> Нормализованный вектор </returns>
+        public Vector Norm()
+        {
+            float len = Len(X, Y, Z);
+            return new Vector(X / len, Y / len, Z / len);
         }
         #endregion
     }
